2f7113aed441dc9ae8c6d5b39492e12b12de9899
GiantQuartz <giantquartz@gmail.com>
2020-07-23 16:24:38 +0200

    Fix NpcDialog->register() not updating the property 'registered'

--- src/NpcDialog/NpcDialog.php
+++ src/NpcDialog/NpcDialog.php
@@ -22,6 +22,7 @@ class NpcDialog {
     static public function register(Plugin $plugin): void {
         if(!self::$registered) {
             $plugin->getServer()->getPluginManager()->registerEvents(new PacketListener(), $plugin);
+            self::$registered = true;
         }
     }
 
