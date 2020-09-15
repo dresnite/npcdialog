536e25ede640cd065bebb15519d73d7f78f09af1
thebigsmileXD <thebigsmilexd@gmail.com>
2020-09-15 16:05:08 +0200

    Add PM4 support

--- src/NpcDialog/PacketListener.php
+++ src/NpcDialog/PacketListener.php
@@ -11,10 +11,10 @@ declare(strict_types=1);
 
 namespace NpcDialog;
 
-
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\NpcRequestPacket;
+use pocketmine\Server;
 
 class PacketListener implements Listener {
 
@@ -23,14 +23,14 @@ class PacketListener implements Listener {
 
     public function onPacketReceiveEvent(DataPacketReceiveEvent $event): void {
         $packet = $event->getPacket();
-        $player = $event->getPlayer();
-        $server = $player->getServer();
+		$server = Server::getInstance();
+		$player = $event->getOrigin()->getPlayer();
 
-        if(!($packet instanceof NpcRequestPacket) or ($entity = $server->findEntity($packet->entityRuntimeId)) === null) {
+        if($player === null || !($packet instanceof NpcRequestPacket) || ($entity = $server->getWorldManager()->findEntity($packet->entityRuntimeId)) === null) {
             return;
         }
 
-        $username = $player->getName();
+		$username = $player->getName();
         $logger = $server->getLogger();
 
         switch($packet->requestType) {
