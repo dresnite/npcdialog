e71240f826401b390d6e5ee8bcb82b0247f37fb4
XenialDan <thebigsmilexd@gmail.com>
2023-03-05 01:47:32 +0100

    Try open on PlayerEntityInteractEvent

--- src/NpcDialog/PacketListener.php
+++ src/NpcDialog/PacketListener.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 namespace NpcDialog;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerEntityInteractEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\NpcDialoguePacket;
 use pocketmine\network\mcpe\protocol\NpcRequestPacket;
 use pocketmine\Server;
 
@@ -23,34 +25,57 @@ class PacketListener implements Listener{
 
 	public function onPacketReceiveEvent(DataPacketReceiveEvent $event) : void{
 		$packet = $event->getPacket();
-		$server = Server::getInstance();
-		$player = $event->getOrigin()->getPlayer();
-
-		if($player === null || !($packet instanceof NpcRequestPacket) || ($entity = $server->getWorldManager()->findEntity($packet->actorRuntimeId)) === null){
-			return;
+		if($packet instanceof NpcRequestPacket){
+			$this->handleNpcRequest($event);
 		}
+	}
 
-		$username = $player->getName();
-		$logger = $server->getLogger();
+	private function handleNpcRequest(DataPacketReceiveEvent $event){
+		/** @var NpcRequestPacket $packet */
+		if(($packet = $event->getPacket()) instanceof NpcRequestPacket){
+			$server = Server::getInstance();
+			$player = $event->getOrigin()->getPlayer();
+			$entity = $server->getWorldManager()->findEntity($packet->actorRuntimeId);
+			if($entity === null) return;
 
-		$logger->debug("Received NpcRequestPacket from $username");
+			$username = $player->getName();
+			$logger = $server->getLogger();
 
-		switch($packet->requestType){
-			case NpcRequestPacket::REQUEST_EXECUTE_ACTION:
-				$logger->debug("Received a NpcRequestPacket action" . $packet->actionIndex);
-				$this->responsePool[$username] = $packet->actionIndex;
-				break;
-			case NpcRequestPacket::REQUEST_EXECUTE_CLOSING_COMMANDS:
-				$form = DialogFormStore::getFormByEntity($entity);
-				if($form !== null){
-					$form->handleResponse($player, $this->responsePool[$username] ?? null);
-					unset($this->responsePool[$username]);
-				}else{
-					$logger->warning("Unhandled NpcRequestPacket for $username because there wasn't a registered form on the store");
+			$logger->debug("Received NpcRequestPacket from $username");
+
+			switch($packet->requestType){
+				case NpcRequestPacket::REQUEST_EXECUTE_ACTION:
+					$logger->debug("Received a NpcRequestPacket action " . $packet->actionIndex);
+					$this->responsePool[$username] = $packet->actionIndex;
+					break;
+				case NpcRequestPacket::REQUEST_EXECUTE_CLOSING_COMMANDS:
+					$form = DialogFormStore::getFormByEntity($entity);
+					if($form !== null){
+						$form->handleResponse($player, $this->responsePool[$username] ?? null);
+						unset($this->responsePool[$username]);
+					}else{
+						$logger->warning("Unhandled NpcRequestPacket for $username because there wasn't a registered form on the store");
+					}
+					break;
+				default:
+				{
+					$logger->warning("Unhandled NpcRequestPacket for $username because the request type was unknown");
+					$logger->debug("NpcRequestPacket request type: " . $packet->requestType . " action index: " . $packet->actionIndex . " command: " . $packet->commandString . " runtime id: " . $packet->actorRuntimeId . " scene name: " . $packet->sceneName);
 				}
-				break;
+			}
+			$event->cancel();
 		}
+	}
 
+	public function onPlayerEntityInteractEvent(PlayerEntityInteractEvent $event) : void{
+		$player = $event->getPlayer();
+		$entity = $event->getEntity();
+		$server = Server::getInstance();
+		$logger = $server->getLogger();
+		$username = $player->getName();
+		$logger->debug("Received PlayerEntityInteractEvent from $username");
+		$pk = NpcDialoguePacket::create($entity->getId(), NpcDialoguePacket::ACTION_OPEN, "hello world", "", $entity->getNameTag(), "");
+		$player->getNetworkSession()->sendDataPacket($pk);
 	}
 
 }
